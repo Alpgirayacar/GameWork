9b1c4c8cae5b7fdc7b3dde50ddcea143d975d906
alpgiray <alpgirayacar@outlook.com>
2024-01-18 00:06:54 +0300

    Menu Manager Button Script Added

--- Assets/Scripts/MenuManagerInGame.cs
+++ Assets/Scripts/MenuManagerInGame.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManagerInGame : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject inGameScreen, pauseScreen;
+
     void Start()
     {
         
@@ -15,4 +19,32 @@ public class MenuManagerInGame : MonoBehaviour
     {
         
     }
+
+    public void PauseButton()
+    {
+        Time.timeScale = 0;
+        inGameScreen.SetActive(false);  
+        pauseScreen.SetActive(true);
+    }
+
+    public void PlayButton()
+    {
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
+        inGameScreen.SetActive(true);   
+         
+    }
+
+    public void RePlayButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    public void HomeButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
 }
